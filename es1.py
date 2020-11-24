0fca4cf35caff086d1ef3a99f43ec1486f1e8f94
unknown <gioia2005caliceti@gmail.com>
2020-11-24 12:09:57 +0100

    svolgo l'esercizio sul file es1.py

---
+++ es1.py
@@ -0,0 +1,6 @@
+votiA = int(input("Quanti voti ha avuto il primo candidato? "))
+votiB = int(input("Quanti voti ha avuto il sceondo candidato? "))
+votiTOT = votiA + votiB
+percA = (votiA*100)/votiTOT
+percB = (votiB*100)/votiTOT
+print("La percentuale dei voti ottenuti dal primo candidato è ",percA, " .La percentuale deo voti ottenuti dal secondo candidato è ",percB, " .")
