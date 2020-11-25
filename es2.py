33ad5a0eb2e2835d1511b3054f2bd7826cf62488
unknown <gioia2005caliceti@gmail.com>
2020-11-25 21:52:45 +0100

    mmodifico l'esercizio sul file es2.py

--- es2.py
+++ es2.py
@@ -1,11 +1,18 @@
-votiprimocandidato = int(input("Quanti voti ha ricevuto il primo candidato? "))
-votisecondocandidato = int(input("Quanti voti ha ricevuto il secondo candidato? "))
+primocandidato = str(input("Nome del primo candidato: "))
+secondocandidato = str(input("Nome del secondo candidato: "))
+
+votiprimocandidato = int(input("Quanti voti ha ricevuto " + primocandidato + " ? "))
+votisecondocandidato = int(input("Quanti voti ha ricevuto " + secondocandidato + " ? "))
+
 punteggio = []
-candidati = ["primo candidato", "secdondo candidato"]
+candidati = []
+candidati.append(primocandidato)
+candidati.append(secondocandidato)
 punteggio.append(votiprimocandidato)
 punteggio.append(votisecondocandidato)
 candidati.sort()
 print("Elenco candidati in ordine alfabetico: ", candidati)
+
 if votiprimocandidato > votisecondocandidato:
     print("Elenco in base al punteggio in ordine decrescente", punteggio)
 elif votisecondocandidato > votiprimocandidato:
